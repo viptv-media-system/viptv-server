0db7027e036c35ec14cfd22c846ff87855700a51
enrell <enrellsa10@proton.me>
2024-06-28 15:21:53 -0300

    update tab size to 4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080
+  	r := gin.Default()
+  	r.GET("/ping", func(c *gin.Context) {
+    	c.JSON(http.StatusOK, gin.H{
+      	"message": "pong",
+    	})
+  	})
+  	r.Run() // listen and serve on 0.0.0.0:8080
 }
